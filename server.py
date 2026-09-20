5e1ef001075d52029cd91fb28d6bb335b6971aca
Ubuntu <ubuntu@ns3115788.ip-54-38-179.eu>
2026-09-20 09:21:51 +0000

    fix: avoid duplicate Acolyte health headers

--- server.py
+++ server.py
@@ -43,7 +43,6 @@ class AcolyteHandler(SimpleHTTPRequestHandler):
         self.send_header("Content-Type", "application/json; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
         self.send_header("Cache-Control", "no-store")
-        self.send_header("X-Robots-Tag", "noindex, nofollow, noarchive")
         self.end_headers()
         self.wfile.write(body)
 
